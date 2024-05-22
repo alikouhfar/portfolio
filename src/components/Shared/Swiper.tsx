2a63f89ebc662eb3e00bccf191de80d8f2f0ac9c
Ali Kouhfar <alikouhfr@gmail.com>
2024-05-22 21:46:58 +0330

    Update: Testimonial Section Disabled

--- src/components/Shared/Swiper.tsx
+++ src/components/Shared/Swiper.tsx
@@ -1,32 +1,32 @@
-// import Swiper core and required modules
-import { Navigation } from "swiper/modules";
+// // import Swiper core and required modules
+// import { Navigation } from "swiper/modules";
 
-import { Swiper, SwiperSlide } from "swiper/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
-import "swiper/css";
-import Testimonial from "../Testimonials/Testimonial";
+// // Import Swiper styles
+// import "swiper/css";
+// import Testimonial from "../Testimonials/Testimonial";
 
-export default function TestimonalSwiper({ testimonials, setActiveSlide }) {
-  return (
-    <Swiper
-      modules={[Navigation]}
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation={{
-        nextEl: "#testimonial-button-next",
-        prevEl: "#testimonial-button-previous",
-      }}
-      onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
-    >
-      {testimonials.map((testimonial, index) => (
-        <SwiperSlide
-          className="rounded-[30px] border-2 border-zinc-600 p-[50px] mobile:p-10"
-          key={index}
-        >
-          <Testimonial testimonial={testimonial} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
-}
+// export default function TestimonalSwiper({ testimonials, setActiveSlide }) {
+//   return (
+//     <Swiper
+//       modules={[Navigation]}
+//       spaceBetween={50}
+//       slidesPerView={1}
+//       navigation={{
+//         nextEl: "#testimonial-button-next",
+//         prevEl: "#testimonial-button-previous",
+//       }}
+//       onSlideChange={(swiper) => setActiveSlide(swiper.activeIndex)}
+//     >
+//       {testimonials.map((testimonial, index) => (
+//         <SwiperSlide
+//           className="rounded-[30px] border-2 border-zinc-600 p-[50px] mobile:p-10"
+//           key={index}
+//         >
+//           <Testimonial testimonial={testimonial} />
+//         </SwiperSlide>
+//       ))}
+//     </Swiper>
+//   );
+// }
