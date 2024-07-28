7e51b2fd1441809da312127a7fc2a068f1408add
Ali Kouhfar <alikouhfr@gmail.com>
2024-07-28 12:27:00 +0330

    Update: Intersection Observer Hook Added

---
+++ app/_hooks/useIntersectionObserver.ts
@@ -0,0 +1,45 @@
+"use client";
+
+import { RefObject, useEffect, useRef, useState } from "react";
+
+interface IUseIntersectionObserver {
+  ref: RefObject<HTMLDivElement>;
+  options: IntersectionObserverInit | undefined;
+}
+
+const useIntersectionObserver = ({
+  ref,
+  options,
+}: IUseIntersectionObserver) => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const observerInit = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      observerInit.current = new window.IntersectionObserver(
+        ([entry]) => setIsIntersecting(entry.isIntersecting),
+        options,
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    const element = ref.current;
+    const observer = observerInit.current;
+
+    if (observer) {
+      if (isIntersecting) {
+        observer.disconnect();
+        return;
+      }
+
+      if (element && !isIntersecting) observer.observe(element);
+
+      return () => observer.disconnect();
+    }
+  }, [isIntersecting, ref]);
+
+  return isIntersecting;
+};
+
+export default useIntersectionObserver;
