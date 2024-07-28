cd632e30be458b4aaa5760a962c7754dd2acfa04
Ali Kouhfar <alikouhfr@gmail.com>
2024-07-28 12:23:52 +0330

    UI Desing: Skills Section Done

---
+++ app/_components/Skills/SkillCard.tsx
@@ -0,0 +1,28 @@
+import { ISkill } from "@/app/_models/skill.model";
+
+interface IProps {
+  skill: ISkill;
+}
+
+export default function SkillCard({ skill }: IProps) {
+  return (
+    <div>
+      <div className="mb-8 px-5 text-zinc-800 transition duration-700 dark:text-zinc-100">
+        <div className="mb-6 flex min-h-12 items-center justify-between gap-4">
+          <h6 className="text-2xl font-semibold leading-6">{skill.title}</h6>
+          <div className="text-lg font-bold">
+            <span>{skill.mastery}</span>
+            <span className="text-primary-green">%</span>
+          </div>
+        </div>
+        <p className="min-h-20 text-justify">{skill.description}</p>
+      </div>
+      <div className="relative h-0.5 bg-zinc-800/10 transition duration-700 dark:bg-zinc-600">
+        <div
+          className="relative block h-0.5 bg-zinc-800 transition duration-700 after:absolute after:-top-3 after:right-0 after:h-6 after:w-6 after:rounded-full after:border-2 after:border-zinc-800 after:bg-primary-green after:shadow-primary-light dark:bg-primary-green dark:after:border-zinc-400 dark:after:bg-card-dark dark:after:shadow-primary-dark"
+          style={{ width: `${skill.mastery}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+}
---
+++ app/_lib/skills.ts
@@ -0,0 +1,202 @@
+import { ISkill } from "../_models/skill.model";
+
+export const skills: ISkill[] = [
+  {
+    title: "HTML/CSS",
+    mastery: 90,
+    description:
+      "Creates semantic HTML structures and applies CSS for responsive design, ensuring cross-device compatibility.",
+  },
+  {
+    title: "JavaScript",
+    mastery: 85,
+    description:
+      "Develops interactive web applications using JavaScript, proficient in modern frameworks like React, Angular, and Vue.js.",
+  },
+  {
+    title: "Responsive Design",
+    mastery: 80,
+    description:
+      "Designs responsive layouts adapting to screen sizes, enhancing user experience across devices.",
+  },
+  {
+    title: "Cross-Browser Compatibility",
+    mastery: 75,
+    description:
+      "Ensures web applications work seamlessly across browsers like Chrome, Firefox, Safari, and Edge.",
+  },
+  {
+    title: "Performance Optimization",
+    mastery: 70,
+    description:
+      "Optimizes website performance through techniques like minification, lazy loading, and API efficiency.",
+  },
+  {
+    title: "Accessibility Compliance",
+    mastery: 65,
+    description:
+      "Ensures web applications are accessible, following WCAG guidelines for users with disabilities.",
+  },
+  {
+    title: "Version Control/Git",
+    mastery: 60,
+    description:
+      "Manages code repositories with Git, facilitating collaboration among developers.",
+  },
+  {
+    title: "RESTful API Integration",
+    mastery: 55,
+    description:
+      "Integrates RESTful APIs for dynamic data fetching and database updates in web applications.",
+  },
+  {
+    title: "Progressive Web Apps (PWA)",
+    mastery: 50,
+    description:
+      "Understands PWAs, including service workers and manifest files for mobile app-like experiences.",
+  },
+  {
+    title: "Testing and Debugging",
+    mastery: 45,
+    description:
+      "Utilizes tools like Jest or Mocha for unit testing and Chrome DevTools for debugging JavaScript errors.",
+  },
+  {
+    title: "React Query",
+    mastery: 60,
+    description:
+      "Uses React Query for efficient server state management and data synchronization in React applications.",
+  },
+  {
+    title: "TypeScript",
+    mastery: 55,
+    description:
+      "Adds static types to React components with TypeScript, enhancing code quality and maintainability.",
+  },
+  {
+    title: "SASS/SCSS",
+    mastery: 65,
+    description:
+      "Styles web applications with SASS/SCSS, utilizing variables, nesting, and mixins for maintainability.",
+  },
+  {
+    title: "Webpack Configuration",
+    mastery: 50,
+    description:
+      "Configures Webpack for bundling JavaScript and assets, tailoring the build process to project needs.",
+  },
+  {
+    title: "Storybook Integration",
+    mastery: 40,
+    description:
+      "Integrates Storybook for isolated UI component development, promoting reuse and faster development.",
+  },
+  {
+    title: "GraphQL Queries",
+    mastery: 45,
+    description:
+      "Queries GraphQL APIs from frontend applications, leveraging GraphQL's efficiency over REST APIs.",
+  },
+  {
+    title: "Unit Testing with Jest",
+    mastery: 55,
+    description:
+      "Writes and runs unit tests with Jest, ensuring code reliability and catching bugs early.",
+  },
+  {
+    title: "Babel Configuration",
+    mastery: 50,
+    description:
+      "Configures Babel for transpiling modern JavaScript to be compatible with older browsers.",
+  },
+  {
+    title: "ES6+ Features",
+    mastery: 70,
+    description:
+      "Utilizes ES6+ features like arrow functions, promises, async/await, and modules for clean, efficient code.",
+  },
+  {
+    title: "Progressive Enhancement",
+    mastery: 60,
+    description:
+      "Applies progressive enhancement principles, ensuring basic content and functionality are universally accessible.",
+  },
+  {
+    title: "React",
+    mastery: 85,
+    description:
+      "Builds user interfaces with React, managing component state and lifecycle methods effectively.",
+  },
+  {
+    title: "TailwindCSS",
+    mastery: 60,
+    description:
+      "Uses TailwindCSS for rapid UI development, employing utility-first classes for streamlined styling.",
+  },
+  {
+    title: "Styled Components",
+    mastery: 55,
+    description:
+      "Stylizes React applications with Styled Components, encapsulating CSS within JavaScript for better reusability.",
+  },
+  {
+    title: "Sass",
+    mastery: 70,
+    description:
+      "Advances Sass usage for readable, maintainable stylesheets with variables, nested rules, and mixins.",
+  },
+  {
+    title: "React Router",
+    mastery: 65,
+    description:
+      "Handles routing in React applications, enabling view navigation and URL parameter management.",
+  },
+  {
+    title: "Next.js",
+    mastery: 55,
+    description:
+      "Utilizes Next.js for server-side rendering and static site generation with React, boosting performance and SEO.",
+  },
+  {
+    title: "Bootstrap",
+    mastery: 45,
+    description:
+      "Leverages Bootstrap for quick prototyping and responsive web design with predefined components.",
+  },
+  {
+    title: "Redux",
+    mastery: 60,
+    description:
+      "Manages state in JavaScript applications with Redux, ensuring state consistency across the app.",
+  },
+  {
+    title: "Redux Toolkit",
+    mastery: 55,
+    description:
+      "Uses Redux Toolkit for simplified Redux development, reducing boilerplate and streamlining common tasks.",
+  },
+  {
+    title: "Performance Optimization Techniques",
+    mastery: 65,
+    description:
+      "Applies performance optimization techniques like code splitting, lazy loading, and image optimization.",
+  },
+  {
+    title: "Accessibility Best Practices",
+    mastery: 60,
+    description:
+      "Adheres to accessibility best practices, ensuring web applications are usable by people with disabilities.",
+  },
+  {
+    title: "Continuous Integration/Continuous Deployment (CI/CD)",
+    mastery: 50,
+    description:
+      "Automates web application deployment with CI/CD pipelines, ensuring reliable and frequent updates.",
+  },
+  {
+    title: "Browser Developer Tools",
+    mastery: 70,
+    description:
+      "Proficiently uses browser developer tools for debugging, profiling, and optimizing web applications.",
+  },
+];
