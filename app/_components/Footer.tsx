3e477aebeaef8355b74a01220a7896a1a987f583
Ali Kouhfar <alikouhfr@gmail.com>
2024-08-01 09:37:49 +0330

    Update: Footer Transition Added

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
   const { theme } = useTheme();
   return (
     <footer
-      className={`${theme === "dark" ? "dark" : ""} bg-gradient-light-center py-16 text-zinc-800 transition duration-700 dark:bg-gradient-dark-center dark:text-zinc-100`}
+      className={`${theme === "dark" ? "dark" : ""} bg-gradient-light-center py-16 text-zinc-800 transition-all duration-700 dark:bg-gradient-dark-center dark:text-zinc-100`}
     >
       <div className="mx-auto grid max-w-[1400px] grid-cols-1 items-center justify-between gap-5 p-3 lg:grid-cols-3">
         <div className="flex justify-center lg:justify-start">
