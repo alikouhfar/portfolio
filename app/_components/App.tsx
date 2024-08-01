936ac67c8c32fa9a8472d30119a57678a5a914c4
Ali Kouhfar <alikouhfr@gmail.com>
2024-08-01 09:36:42 +0330

    Update: App Component Structure Changed

--- app/_components/App.tsx
+++ app/_components/App.tsx
@@ -1,14 +1,5 @@
-import { ThemeProvider } from "@/app/_contexts/ThemeContext";
-import Footer from "./Footer";
-import Header from "./Header";
 import Main from "./Main";
 
 export default function App() {
-  return (
-    <ThemeProvider>
-      <Header />
-      <Main />
-      <Footer />
-    </ThemeProvider>
-  );
+  return <Main />;
 }
