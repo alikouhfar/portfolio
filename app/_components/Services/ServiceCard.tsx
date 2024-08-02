fb5c31dd9f1e3acd150efa93df00ab32f1189d3d
Ali Kouhfar <alikouhfr@gmail.com>
2024-08-02 11:43:28 +0330

    UI Design: Category Color Changed

--- app/_components/Services/ServiceCard.tsx
+++ app/_components/Services/ServiceCard.tsx
@@ -11,16 +11,16 @@ interface IProps {
 export default function ServiceCard({ service }: IProps) {
   return (
     <div className="relative flex h-full flex-col overflow-hidden rounded-2xl bg-white p-8 text-zinc-800 opacity-100 shadow transition duration-700 dark:bg-card-dark dark:text-zinc-100">
-      <div className="mb-16 text-[13px] font-medium uppercase text-zinc-800 transition duration-700 dark:text-primary-green">
-        <span>{service.category}</span>
-      </div>
+      <span className="mb-16 text-[13px] font-semibold uppercase text-primary-green">
+        {service.category}
+      </span>
       <div className="flex flex-1 flex-col justify-between">
         <h5 className="mb-4 text-2xl font-bold leading-6">{service.title}</h5>
         <div>
           <p className="min-h-44 leading-7 opacity-80">{service.description}</p>
           <Link
             href="https://luique.bslthemes.com/#pricing-section"
-            className="relative flex gap-3 align-top text-base font-bold leading-6 text-zinc-800 transition-all duration-700 hover:gap-2 hover:text-primary-green dark:text-zinc-100"
+            className="relative flex gap-3 align-top text-base font-bold leading-6 text-zinc-800 transition-all duration-500 hover:gap-2 hover:text-primary-green dark:text-zinc-100"
           >
             <span>See Pricing</span>
             <div>
