c449a57880dfc125778bb0c2adc912aeee1b1e92
Ali Kouhfar <alikouhfr@gmail.com>
2024-08-02 11:47:04 +0330

    Update: Moved The Main Component's Content to App

--- app/_components/App.tsx
+++ app/_components/App.tsx
@@ -1,5 +1,68 @@
-import Main from "./Main";
+"use client";
+
+import Contact from "./Contact/Contact";
+import Hero from "./Hero";
+import { GridPortfolio } from "./Portfolio/Portfolio";
+import Pricing from "./Pricing/Pricing";
+import Resume from "./Resume/Resume";
+import SectionWrapper from "./SectionWrapper";
+import Services from "./Services/Services";
+import ProfessionalSkills from "./Skills/ProfessionalSkills";
+import Testimonials from "./Testimonials/Testimonials";
 
 export default function App() {
-  return <Main />;
+  return (
+    <>
+      <Hero />
+      <SectionWrapper
+        component={<Services />}
+        title="What I do"
+        subtitlePhrase="My"
+        subtitle="Services"
+        gradientPosition="bottom"
+      />
+      <SectionWrapper
+        component={<ProfessionalSkills />}
+        title="Professional Skills"
+        subtitlePhrase="My"
+        subtitle="Talent"
+        gradientPosition="center"
+      />
+      <SectionWrapper
+        component={<GridPortfolio />}
+        title="Portfolio"
+        subtitlePhrase="My"
+        subtitle="Cases"
+        gradientPosition="top"
+      />
+      <SectionWrapper
+        component={<Resume />}
+        title="Resume"
+        subtitlePhrase="My"
+        subtitle="Story"
+        gradientPosition="bottom"
+      />
+      <SectionWrapper
+        component={<Testimonials />}
+        title="Testimonials"
+        subtitlePhrase="What"
+        subtitle="Customers Say"
+        gradientPosition="center"
+      />
+      <SectionWrapper
+        component={<Pricing />}
+        title="Pricing"
+        subtitlePhrase="My"
+        subtitle="Price Board"
+        gradientPosition="top"
+      />
+      <SectionWrapper
+        component={<Contact />}
+        title="Contact Me"
+        subtitlePhrase="Let's"
+        subtitle="Talk About Ideas"
+        gradientPosition="bottom"
+      />
+    </>
+  );
 }
