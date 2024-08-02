7c457eb6bc9a6d79d6ac1aa92ffe72472bf23c54
Ali Kouhfar <alikouhfr@gmail.com>
2024-08-02 11:39:52 +0330

    Update: Added Id to Service Model

--- app/_lib/services.ts
+++ app/_lib/services.ts
@@ -2,90 +2,105 @@ import { IService } from "../_models/service.model";
 
 export const services: IService[] = [
   {
+    id: 1,
     category: "UI/UX Design",
     title: "Custom UI/UX Design",
     description:
       "Designs intuitive interfaces based on user behavior, focusing on responsive layouts for enhanced user experience.",
   },
   {
+    id: 2,
     category: "Web Development",
     title: "Full-Stack Web Development",
     description:
       "Develops dynamic websites and applications using modern tech stacks like React, Node.js, and MongoDB.",
   },
   {
+    id: 3,
     category: "Performance Optimization",
     title: "Website Performance Enhancement",
     description:
       "Optimizes website speed and efficiency through code optimization and caching strategies.",
   },
   {
+    id: 4,
     category: "Accessibility Compliance",
     title: "Web Accessibility Consulting",
     description:
       "Ensures websites are accessible to all users by following WCAG guidelines and implementing alternative texts and keyboard navigation.",
   },
   {
+    id: 5,
     category: "Cross-Browser Compatibility Testing",
     title: "Cross-Browser Testing",
     description:
       "Tests websites on multiple browsers to ensure compatibility and resolve any identified issues.",
   },
   {
+    id: 6,
     category: "Progressive Web App (PWA) Development",
     title: "PWA Development",
     description:
       "Creates installable web applications with offline capabilities and push notifications for an app-like experience.",
   },
   {
+    id: 7,
     category: "E-commerce Solutions",
     title: "E-commerce Frontend Development",
     description:
       "Develops engaging online shopping experiences with product pages, carts, and checkout processes.",
   },
   {
+    id: 8,
     category: "Mobile Responsiveness",
     title: "Mobile-Friendly Web Design",
     description:
       "Ensures websites automatically adjust to fit various screen sizes for a positive mobile experience.",
   },
   {
+    id: 9,
     category: "Security Auditing",
     title: "Web Security Auditing",
     description:
       "Assesses websites for vulnerabilities and implements fixes to protect data and maintain user trust.",
   },
   {
+    id: 10,
     category: "Animation",
     title: "Interactive Web Animations",
     description:
       "Enhances user engagement with animated elements and transitions for a dynamic interface.",
   },
   {
+    id: 11,
     category: "Internationalization",
     title: "Localization and Intl.",
     description:
       "Adapts websites for global audiences by translating content and adjusting designs for cultural nuances.",
   },
   {
+    id: 12,
     category: "SEO Optimization",
     title: "SEO Services",
     description:
       "Improves website visibility in search engines through content optimization and structural enhancements.",
   },
   {
+    id: 13,
     category: "Maintenance and Support",
     title: "Website Maintenance",
     description:
       "Provides ongoing updates, bug fixes, and security patches to keep websites current and secure.",
   },
   {
+    id: 14,
     category: "Content Management System (CMS) Integration",
     title: "CMS Integration",
     description:
       "Sets up CMS platforms and customizes themes and plugins for easy content management.",
   },
   {
+    id: 15,
     category: "Testing and Quality Assurance",
     title: "Comprehensive Testing",
     description:
--- app/_models/service.model.ts
+++ app/_models/service.model.ts
@@ -1,4 +1,5 @@
 export interface IService {
+  id: number;
   category: string;
   title: string;
   description: string;
