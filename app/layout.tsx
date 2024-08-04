9aa45bc3d4ce99df68d886e913af55662052476c
Ali Kouhfar <alikouhfr@gmail.com>
2024-08-04 22:12:46 +0330

    Update: Added Dynamic Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,10 @@ import { jost } from "./_ui/fonts";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Portfolio of Ali Kouhfar",
+  title: {
+    default: "Portfolio of Ali Kouhfar",
+    template: "Ali Kouhfar ◾ %s",
+  },
   description:
     "Portfilio is a sleek portfolio website showcasing my skills, projects, and experience. Built with modern web technologies, including HTML, CSS, JavaScript, and React this website is designed to impress potential employers, clients, and collaborators. With a responsive layout, it looks great on any device, from desktops to smartphones.",
 };
--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import Works from "../_components/Works";
+
+export const metadata: Metadata = {
+  title: "Works",
+};
 
 export default function Page() {
   return <Works />;
