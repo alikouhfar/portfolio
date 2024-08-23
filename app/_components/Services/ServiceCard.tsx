522217e41e33277c7f470c6e11ed18b860131bf0
Ali Kouhfar <alikouhfr@gmail.com>
2024-08-23 21:48:34 +0330

    Update: Service Card UI Updated

--- app/_components/Services/ServiceCard.tsx
+++ app/_components/Services/ServiceCard.tsx
@@ -12,13 +12,15 @@ interface IServiceCardProps {
 const ServiceCard: FC<IServiceCardProps> = ({ service }) => {
   return (
     <div className="relative flex h-full flex-col overflow-hidden rounded-2xl bg-white p-8 text-zinc-800 opacity-100 shadow transition duration-700 dark:bg-card-dark dark:text-zinc-100">
-      <span className="mb-16 text-[13px] font-semibold uppercase text-primary-green">
+      <span className="mb-8 text-[13px] font-semibold uppercase text-primary-green sm:mb-16">
         {service.category}
       </span>
       <div className="flex flex-1 flex-col justify-between">
         <h5 className="mb-4 text-2xl font-bold leading-6">{service.title}</h5>
         <div>
-          <p className="min-h-44 leading-7 opacity-80">{service.description}</p>
+          <p className="min-h-52 leading-7 opacity-80 sm:min-h-44">
+            {service.description}
+          </p>
           <Link
             href="https://luique.bslthemes.com/#pricing-section"
             className="relative flex gap-3 align-top text-base font-bold leading-6 text-zinc-800 transition-all duration-500 hover:gap-2 hover:text-primary-green dark:text-zinc-100"
