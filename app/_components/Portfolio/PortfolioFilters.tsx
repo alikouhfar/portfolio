7219bf44039f7344e1ac19c66441eb19592a0d4e
Ali Kouhfar <alikouhfr@gmail.com>
2024-08-23 21:46:59 +0330

    Update: Portfolio Filters Logic Changed

--- app/_components/Portfolio/PortfolioFilters.tsx
+++ app/_components/Portfolio/PortfolioFilters.tsx
@@ -4,34 +4,35 @@ import useSearchParam from "@/app/_hooks/useSearchParam";
 import { projects } from "@/app/_lib/projects";
 import { FC } from "react";
 
-interface IProps {
+interface IFilterButtonProps {
   param: string;
   content: string;
 }
 
+const categories = new Set(projects.map((project) => project.category));
+const filters = Array.from(categories);
+
 const PortfolioFilters: FC = () => {
   const tags = new Set(projects.map((project) => project.tag));
   const params = Array.from(tags);
-  const categories = new Set(projects.map((project) => project.category));
-  const filters = Array.from(categories);
 
   return (
     <div className="mb-16 flex flex-col justify-center gap-8 sm:flex-row sm:gap-10">
-      <FilterButton content="All" param="all" />
+      {filters.length > 1 && <FilterButton content="All" param="all" />}
       {filters.map((filter, index) => (
-        <FilterButton key={index} content={filter} param={params[index]} />
+        <FilterButton key={index} param={params[index]} content={filter} />
       ))}
     </div>
   );
 };
 
-function FilterButton({ param, content }: IProps) {
+function FilterButton({ param, content }: IFilterButtonProps) {
   const { activeParam, setParam } = useSearchParam("works", "all");
 
   return (
     <button
       onClick={() => setParam(param)}
-      className={`${param === activeParam ? "text-primary-green after:w-10" : ""} relative font-semibold text-zinc-800 transition-all duration-700 after:absolute after:bottom-1/2 after:left-0 after:h-0.5 after:w-0 after:max-w-[60%] after:-translate-y-1/2 after:bg-primary-green after:transition-all after:duration-300 after:content-[''] hover:after:w-10 sm:after:-bottom-3 dark:text-zinc-100`}
+      className={`${param === activeParam || filters?.length === 1 ? "text-primary-green after:w-10" : ""} relative font-semibold text-zinc-800 transition-all duration-700 after:absolute after:bottom-1/2 after:left-0 after:h-0.5 after:w-0 after:max-w-[60%] after:-translate-y-1/2 after:bg-primary-green after:transition-all after:duration-300 after:content-[''] hover:after:w-10 sm:after:-bottom-3 dark:text-zinc-100`}
     >
       {content}
     </button>
