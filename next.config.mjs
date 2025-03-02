2bb0d877aba4686843ba6faaf9227577d1df5efd
Ali Kouhfar <alikouhfr@gmail.com>
2025-03-02 21:01:00 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/portfolio",
+  output: "export",
+  reactStrictMode: true,
+};
 
-export default nextConfig;
+module.exports = nextConfig;
